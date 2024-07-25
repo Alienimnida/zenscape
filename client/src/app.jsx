67d9fb47a651bd1510c5fa8c01f216cc3d67b3bc
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2024-07-25 18:41:01 +0530

    Update app.jsx

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from 'react';
 import Home from "./pages/Home";
 import LandingPage from "./pages/LandingPage";
 import Results from "./pages/Results";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Modal from './components/Modal';
 import SpotifyCallback from './components/SpotifyCallback';
+
+
 const App = () => {
     const [modalType, setModalType] = useState(null);
 
@@ -16,16 +21,37 @@ const App = () => {
         setModalType(null);
     };
 
+
+    const router = createBrowserRouter([
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/music",
+        element: <Home />,
+      },
+      {
+        path: "/results",
+        element: <Results />,
+      },
+      {
+        path: "/callback",
+        element: <SpotifyCallback />
+      }
+    ]);
+
     return (
         <BrowserRouter>
             <div>
                 {modalType && <Modal type={modalType} closeModal={closeModal} />}
-                <Routes>
+{/*                 <Routes>
                     <Route path={"/"} element={<LandingPage />} />
                     <Route path={"/music"} element={<Home />} />
                     <Route path="/results" element={<Results />} />
                     <Route path="/callback" element={<SpotifyCallback />} />
-                </Routes>
+                </Routes> */}
+                <RouterProvider router={router} />
             </div>
         </BrowserRouter>
     );
