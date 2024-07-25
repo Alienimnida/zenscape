f831509b11db10b03171f1d8c07d613bebcd2b2e
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2024-07-25 19:21:50 +0530

    fix: router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,15 +7,5 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/*",
-    element: <App />,
-  },
-]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
