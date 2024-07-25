4918a1b57c79d679d704a6cfb3381ca86cae2715
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2024-07-25 19:24:29 +0530

    s

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,32 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import SpotifyCallback from './components/SpotifyCallback';
+import Home from "./pages/Home";
+import LandingPage from "./pages/LandingPage";
+import Results from "./pages/Results";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/music",
+    element: <Home />,
+  },
+  {
+    path: "/results",
+    element: <Results />,
+  },
+  {
+    path: "/callback",
+    element: <SpotifyCallback />
+  }
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode >
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
