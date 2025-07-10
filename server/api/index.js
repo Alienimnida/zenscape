d74eadad8dd3b66368379a32a9f4bb3cbd580a17
Ankita Chakraborty <ankitachakraborty2003@gmail.com>
2025-07-10 15:42:31 +0530

    minor change

--- server/api/index.js
+++ server/api/index.js
@@ -39,7 +39,7 @@ function hashUserId(userId) {
 }
 
 app.get('/', async (req, res) => {
-    res.send("Hello world");
+    res.send("Hello world!!!");
 });
 
 // Endpoint to handle recommendation requests
